025dba3c1f7946e9acd3504b2e4f0f48fc5f16d5
MMarcel02 <marciniak.marc@gmail.com>
2025-09-10 12:44:24 +0200

    Starting source code

---
+++ Jerry's Game - Mini project/Game.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Game {
+	private static Scanner scanner;
+	//This is an optional assignment
+	public static void main(String[] args) {
+		// ----- Correct the code below
+		System.out.println(
+				"	         __                    _          ___       __                 __                 \n"
+						+ "		/ /__  ____________  _( )_____   /   | ____/ /   _____  ____  / /___  __________  \n"
+						+ "	   __  / / _ \\/ ___/ ___/ / / /// ___/  / /| |/ __  / | / / _ \\/ __ \\/ __/ / / / ___/ _ \\\n"
+						+ "	  / /_/ /  __/ /  / /  / /_/ / (__  )  / ___ / /_/ /| |/ /  __/ / / / /_/ /_/ / /  /  __/ \n"
+						+ "	  \\____/\\___/_/  /_/   \\__, / /____/  /_/  |_\\__,_/ |___/\\___/_/ /_/\\__/\\__,_/_/   \\___/  \n"
+						+ "			      /____/  	by: Student\n");
+		// ----- Correct the code above
+
+		scanner = new Scanner(System.in);
+		// ----- Write your code below
+
+		// ----- Write your code above
+	}
+
+	// public static int calculateAge(int birthYear) {
+	// // ----- Write your code below
+
+	// // ----- Write your code above
+	// }
+}
